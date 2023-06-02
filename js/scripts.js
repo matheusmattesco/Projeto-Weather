0536aa403b882e280481315e832231d0f5cfdaec
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-06-02 14:16:16 -0300

    Script Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,52 @@
-const apiKey = "26028e87e3290021dda6d400449f9483";
-const apiContryURL = "https://flagsapi.com/BR/flat/64.png";
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = "26028e87e3290021dda6d400449f9483";
+    const apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
+  
+    const cityInput = document.querySelector("#city-input");
+    const searchBtn = document.querySelector("#search");
 
-const cityInput = document.querySelector("#city-input");
-const searchBtn = document.querySelector("#search");
 
-//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=
+    const cityElement = document.querySelector("#city");
+    const tempElement = document.querySelector("#temperature span");
+    const descElement = document.querySelector("#description");
+    const weatherIconElement = document.querySelector("#weather-icon");
+    const countryElement = document.querySelector("#country");
+    const humidityElement = document.querySelector("#humidity span");
+    const windElement = document.querySelector("#wind span");
+  
+    // Funções
+    const getWeatherData = async(city) => {
+        const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    
+         const res = await fetch(apiWeatherURL);
+         const data = await res.json();
+
+         return data;  
+    };
+
+    const showWeatherData = async (city) => {
+        const data = await getWeatherData(city);
+
+        cityElement.innerText = data.name;
+        tempElement.innerText = parseInt(data.main.temp);
+        descElement.innerText = data.weather[0].description;
+        weatherIconElement.setAttribute(
+            "src", 
+            `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        );
+        countryElement.setAttribute("src", 
+        `https://flagsapi.com/${data.sys.country}/flat/64.png`
+        );
+        humidityElement.innerText = `${data.main.humidity}%`;
+        windElement.innerText = `${data.wind.speed}km/h`;
+    };
+
+    //Eventos
+    searchBtn.addEventListener("click", function(e) {
+      e.preventDefault();
+      
+      const city = cityInput.value;
+
+      showWeatherData(city); 
+    });
+  });
