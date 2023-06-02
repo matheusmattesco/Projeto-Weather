8a63e28506bb1fee7cd89abef51734e627051dca
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-06-02 11:14:00 -0300

    Script CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-// 26028e87e3290021dda6d400449f9483
+const apiKey = "26028e87e3290021dda6d400449f9483";
+const apiContryURL = "https://flagsapi.com/BR/flat/64.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
 
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=
