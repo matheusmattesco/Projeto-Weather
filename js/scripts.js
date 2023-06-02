7b53a763d21a5f80a6cc41795d4a44871481f6fa
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-06-02 14:31:23 -0300

    Projeto quase Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const apiKey = "26028e87e3290021dda6d400449f9483";
-    const apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
   
     const cityInput = document.querySelector("#city-input");
     const searchBtn = document.querySelector("#search");
@@ -14,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const humidityElement = document.querySelector("#humidity span");
     const windElement = document.querySelector("#wind span");
   
+    const weatherContainer = document.querySelector("#weather-data");
     // Funções
     const getWeatherData = async(city) => {
         const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
@@ -39,6 +39,8 @@ document.addEventListener("DOMContentLoaded", function() {
         );
         humidityElement.innerText = `${data.main.humidity}%`;
         windElement.innerText = `${data.wind.speed}km/h`;
+
+        weatherContainer.classList.remove("hide");
     };
 
     //Eventos
@@ -48,5 +50,14 @@ document.addEventListener("DOMContentLoaded", function() {
       const city = cityInput.value;
 
       showWeatherData(city); 
+    });
+
+    cityInput.addEventListener("keyup", (e) => {
+
+        if(e.code === "Enter") {
+            const city = e.target.value
+
+            showWeatherData(city);
+        }
     });
   });
